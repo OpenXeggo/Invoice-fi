feced468cfd74cab537e4a5d1b8cbac757fa4b1f
mmajudith <mmajudith2000@gmail.com>
2022-06-23 00:53:43 -0700

    feat
    Validated the whole input fields and added share button that allowed to share the generated link

--- src/Pages/CreateInvoice.jsx
+++ src/Pages/CreateInvoice.jsx
@@ -1,6 +1,9 @@
 import React,{useState} from 'react'
+import { initWeb3 } from '../utils/init'
 
 const CreateInvoice = ({contract,account}) => {
+    const [getInvoiceId, setGetInvoiceId] = useState()
+    const [isSuccess, setIsSuccsee] = useState(false)
     const [invoiceData, setinvoiceData] = useState({
         tokenAddress:"",
         tokenAmount:0,
@@ -14,8 +17,35 @@ const CreateInvoice = ({contract,account}) => {
 
     const onSubmitHandler=async(e)=>{
             e.preventDefault();
-            await contract.methods.createInvoice(invoiceData.tokenAddress,invoiceData.tokenAmount,invoiceData.receiverAddress).send({from:account})
-    }
+
+            let senderAddress = initWeb3().utils.isAddress(invoiceData.tokenAddress)
+            let receiverAddress = initWeb3().utils.isAddress(invoiceData.receiverAddress)
+
+            if(!senderAddress){
+                alert('Please enter valid token Address!')
+                return
+            }
+
+            if(invoiceData.tokenAmount === 0){
+                alert('Please enter a valid token amount!')
+                return
+            }
+
+            if(!receiverAddress){
+                alert('Please enter valid receiver Address!')
+                return
+            }
+
+            let str = invoiceData.tokenAmount.toString()
+
+            let convertToWei = initWeb3().utils.toWei(str, "ether");
+
+            await contract.methods.createInvoice(invoiceData.tokenAddress,convertToWei,invoiceData.receiverAddress).send({from:account})
+            let nextInvoiceId = await contract.methods.getNextInvoiceID().call()
+            console.log('next', nextInvoiceId)
+            setGetInvoiceId(nextInvoiceId)
+            setIsSuccsee(true)
+    }        
   return (
     <div className="p-4 border-4 border-blue-300">
         <h1>Create Invoice</h1>
@@ -33,6 +63,16 @@ const CreateInvoice = ({contract,account}) => {
                 <input type="text" id="receiverAddress" value={invoiceData.receiverAddress} onChange={(e)=>onChangeHandler(e)} />
                 </div>
             <button type="submit">Create Invoice</button>
+            {isSuccess && (
+                <button>
+                    <a href={`http://localhost:3000/invoice${getInvoiceId}`}
+                        target='_blank'
+                        rel='noopener noreferrer'
+                    >
+                        share
+                    </a>
+            </button>
+            )}
         </form>
     </div>
   )
