57b4b72b5fcac34c8595c67a3d361fdcef14c6dd
JoshBot <joshbot04@gmail.com>
2022-07-03 16:38:54 +0100

    Sidebar and Navbar completed

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,6 +3,12 @@ import XeggoLogo from '../assets/Xeggo.png';
 import DashboardIcon from "../assets/dashboard.png";
 import LetterIcon from "../assets/letter.png"
 import { modifyAddress } from "../utils/modifyAddress";
+import BellIcon from "../assets/bell.svg"
+import ProfileIcon from "../assets/profile.svg"
+import MoonIcon from "../assets/icons/MoonIcon";
+import SunIcon from "../assets/icons/SunIcon"
+
+import SearchIcon from "../assets/search.svg"
 
 const Navbar = ({account}) => {
 
@@ -11,6 +17,32 @@ const Navbar = ({account}) => {
             <div className="navbar-container">
                 <div className="logo-container pointer" onClick={e=>window.location.reload()}>
                     <span className="logo"><img src={XeggoLogo} alt="Xeggo Logo" /></span>
+                </div>
+                <div className="header-container">
+                    <div className="search-container">
+                        <img src={SearchIcon} alt="" />
+                        <input type="input" className="search-bar" placeholder="Search Invoice"/>
+                    </div>
+                    <div className="header-end">
+                        <div className="flex"><img src={BellIcon} /></div>
+                            <span className="gap">|</span>
+                        <div className="flex gap-10">
+                            <img src={ProfileIcon} />
+                            <div className="profile flex flex-col justify-center">
+                                <b>Xeggo</b>
+                                <span>03x...1919</span>
+                            </div>
+                        </div>
+                        <span className="gap">|</span>
+                        <div className="switch">
+                            <div className="active">
+                                <SunIcon />
+                            </div>
+                            <div className="">
+                                <MoonIcon />
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
