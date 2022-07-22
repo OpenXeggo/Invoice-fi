613a8f8b58e913aca7507e4ee60cf6cfc764438c
aman-webdev <aman.nabi02@gmail.com>
2022-07-22 12:30:56 +0530

    checkifuserexists in db

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { MoralisProvider } from "react-moralis";
 import { BrowserRouter } from "react-router-dom";
-import App from './App';
-import './index.css';
+import App from "./App";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <MoralisProvider
+      serverUrl="https://ggorfgpqkaxw.usemoralis.com:2053/server"
+      appId="Dyas1VFcb3V2V3MSSEeHHABHAq4dXNTX0Tk1NVS8"
+    >
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </MoralisProvider>
   </React.StrictMode>
 );
